086082a5f67c139b440ea9c161afef66cc97d904
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-10-10 12:44:43 -0400

    Create credits.sh

---
+++ CUT/credits.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. usr/local/CUT/common.sh
+
+credits () {
+  clear
+  logo
+  echo "Licensed under the GPLv3 license (due to Shimboot's license)"
+  echo "User-facing scripts and Shimboot modifications - Rosa Green"
+  echo "SH1MMER exploit discovery - Mercury Workshop"
+  echo "Shimboot - VK6, or ading2210"
+  echo "Alpine - Alpine Linux project"
+  echo "Shflags - Kate Ward"
+  echo "VBOOT utilities - ChromiumOS project"
+  echo "Flashrom - Flashrom project, ChromiumOS project"
+  echo "Logo - Vale#0210"
+  echo "Information and fact-checking for docs - Hannah (@zeglol), Katelyn (@kxtzownsu)"
+  read a
+}
