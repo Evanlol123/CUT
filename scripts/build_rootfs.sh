ae5d0f0ac9d408bf89f0467ea5734295e1cad0bd
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-11-03 21:24:31 -0500

    Update build_rootfs.sh

--- scripts/build_rootfs.sh
+++ scripts/build_rootfs.sh
@@ -75,7 +75,7 @@ if [ ! -d shflags ]; then
 fi
 
 print_info "Building auxilary binaries because ChromeOS just has to be unique"
-chmod +x ./buildables/flashrom.sh
+chmod +x ./buildables/*
 
 buildables="flashrom vpd"
 for buildscript in $buildables 
